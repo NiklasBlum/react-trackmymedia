5a86647ba64c86186498dc00360bcf88032a5d8a
Niklas Blum <niklas.blum97@gmail.com>
2023-03-02 18:26:26 +0100

    added sounds

--- src/components/WatchlistState.tsx
+++ src/components/WatchlistState.tsx
@@ -6,21 +6,33 @@ import { useMediaStore } from "../store";
 import MediaItem from "../types/MediaItem";
 import { LoadingButton } from "@mui/lab";
 import { toast } from "react-toastify";
+import useSound from "use-sound";
 
 export default function WatchlistState({ mediaItem }: { mediaItem: MediaItem }): JSX.Element {
 
     const [isWatchlist, setIsWatchlist] = useState(false);
     const [isLoading, setLoading] = useState<boolean>(false);
     const { mediaType } = useMediaStore();
+    const [playSuccess] = useSound('/sounds/Success.mp3', {  volume: 0.25, });
+    const [playError] = useSound('/sounds/Error.mp3', { volume: 0.25 });
 
     async function setWatchlistToState(state: boolean) {
-        setLoading(true);
-        await sleep(200);
-        await setWatchlistState(mediaItem, mediaType, state)
-        setIsWatchlist(state);
-        setLoading(false);
 
-        toast.success(state ? `${mediaItem.title} Added To Watchlist` : `${mediaItem.title} Removed From Watchlist`);
+        setLoading(true);
+
+        try {
+            await sleep(200);
+            await setWatchlistState(mediaItem, mediaType, state);
+            setIsWatchlist(state);
+            setLoading(false);
+            state ? playSuccess() : playError();
+            toast.success(state ? `${mediaItem.title} Added To Watchlist` : `${mediaItem.title} Removed From Watchlist`);
+        } catch (error) {
+            toast.error(error);
+        }
+        finally {
+            setLoading(false);
+        }
     }
 
     function sleep(ms) {
---
+++ src/views/MediaDetails.tsx
@@ -0,0 +1,3 @@
+export default function MediaDetails() {
+    return (<div>TBI</div>)
+}
